d7493fd6eb5ffd12f46b1b840a2bb7493396b18a
Morten Devold <morten@seats.io>
2025-01-15 15:11:18 +0100

    Add stylistic ESLint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,27 @@
 // @ts-check
 import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin'
 import tseslint from 'typescript-eslint';
 import globals from 'globals'
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  {
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        }
-    },
-    rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    {
+        plugins: {
+            '@stylistic': stylistic
+        },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            }
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off'
+        },
     }
-  }
 ).concat({
-  ignores: ['release.mjs'],
+    ignores: ['release.mjs'],
 })
